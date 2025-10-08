45b981462e86d145a6a2481f9e22952f102ef8b1
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-10-08 16:30:02 -0300

    About page mobile done

---
+++ src/data/professionals.js
@@ -0,0 +1,27 @@
+import first from "../assets/images/professionals/1.png";
+import second from "../assets/images/professionals/2.png";
+import third from "../assets/images/professionals/3.png";
+
+export const professionals = [
+  {
+    name: "joaquín Cárdenas",
+    role: "ceo",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    image: first,
+  },
+  {
+    name: "juan gonzalez",
+    role: "ceo",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    image: second,
+  },
+  {
+    name: "pedro alvarez",
+    role: "ceo",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    image: third,
+  },
+];
---
+++ src/data/values.js
@@ -0,0 +1,26 @@
+export const values = [
+  {
+    title: "seguridad",
+    subtitle: "Cumplimos normas IRAM y protocolos de seguridad",
+  },
+  {
+    title: "calidad",
+    subtitle: "Componentes de primeras marcas, procesos controlados",
+  },
+  {
+    title: "compromiso",
+    subtitle: "Atención personalizada para cada proyecto",
+  },
+  {
+    title: "responsabilidad",
+    subtitle: "Cumplimos plazos y nos hacemos cargo de cada proyecto",
+  },
+  {
+    title: "sostenibilidad",
+    subtitle: "Procesos eficientes y respetuosos con el medio ambiente",
+  },
+  {
+    title: "innovación",
+    subtitle: "Soluciones a medida para la industria moderna",
+  },
+];
