3cddcc211602dfa6e95bcc26807182c812d404fb
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-10-11 17:16:19 -0300

    add property hasIcon to section title component

---
+++ src/data/contact.js
@@ -0,0 +1,45 @@
+import LocationIcon from "../assets/icons/location.png";
+import PhoneIcon from "../assets/icons/phone-call.png";
+import EmailIcon from "../assets/icons/mail.png";
+import InstagramIcon from "../assets/icons/instagram.png";
+import FacebookIcon from "../assets/icons/facebook.png";
+import TwitterIcon from "../assets/icons/twitter.png";
+
+export const information = [
+  {
+    title: "Dirección",
+    firstLine: "Calle falsa 123",
+    secondLine: "Tandil, Argentina",
+    icon: LocationIcon,
+  },
+  {
+    title: "Teléfonos",
+    firstLine: "(2494) - 123456",
+    secondLine: "(2494) - 789987",
+    icon: PhoneIcon,
+  },
+  {
+    title: "E-mail",
+    firstLine: "email.falso@sitec.com",
+    secondLine: null,
+    icon: EmailIcon,
+  },
+  {
+    title: "Instagram",
+    firstLine: "/sitec-instagram",
+    secondLine: null,
+    icon: InstagramIcon,
+  },
+  {
+    title: "Facebook",
+    firstLine: "/sitec-facebook",
+    secondLine: null,
+    icon: FacebookIcon,
+  },
+  {
+    title: "Twitter",
+    firstLine: "/sitec-twitter",
+    secondLine: null,
+    icon: TwitterIcon,
+  },
+];
